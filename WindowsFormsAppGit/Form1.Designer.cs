7b08c33a9e660ca652473b2de11fb1c00dc2dda0
svetamazz <swetamazkiw@gmail.com>
2019-05-21 19:36:42 +0300

    RRRRRRRRRRRRR

--- WindowsFormsAppGit/Form1.Designer.cs
+++ WindowsFormsAppGit/Form1.Designer.cs
@@ -76,7 +76,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(152, 199);
+            this.button2.Location = new System.Drawing.Point(152, 191);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
